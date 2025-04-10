48238a97769d6ba5ee9f46f1aeaf840b001987eb
SheharaJ <sheharajaya7@gmail.com>
2025-04-10 13:33:41 +0530

    Initial Commit

---
+++ ThriveTime/ThriveTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThriveTimeApp.swift
+//  ThriveTime
+//
+//  Created by Shehara Jayasooriya on 2025-04-10.
+//
+
+import SwiftUI
+
+@main
+struct ThriveTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThriveTimeTests/ThriveTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThriveTimeTests.swift
+//  ThriveTimeTests
+//
+//  Created by Shehara Jayasooriya on 2025-04-10.
+//
+
+import Testing
+@testable import ThriveTime
+
+struct ThriveTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
